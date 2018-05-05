b1c94a3ea186903564791cf3b77c3f74644455a1
denarced <denarced@gmail.com>
2018-05-05 21:09:50 +0300

    Modify perf test to create more directories.

--- perf_test.sh
+++ perf_test.sh
@@ -10,9 +10,16 @@ mkdir $dirpath
 
 echo "Generate the test files (takes a few secs)"
 # Work around argument list length restrictions
-for each in {1800..1999}
+for year in {1800..1999}
 do
-    touch $dirpath/main_$each-{01..12}-{01..28}.log
+    yearDir="${dirpath}/${year}"
+    mkdir "$yearDir"
+    for month in {01..12}
+    do
+        monthDir="${yearDir}/${month}"
+        mkdir $monthDir
+        touch ${monthDir}/main_${year}-${month}-{01..28}.log
+    done
 done
 echo "Find 4 files among `ls $dirpath | wc -l`"
 time ./fdated.py -n 100 -o 95 -t 2000-01-01 $dirpath >/dev/null
