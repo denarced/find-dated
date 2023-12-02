2f767698d2fb9694ce73d6305aa279d624774258
denarced <denarced@gmail.com>
2023-12-02 20:34:58 +0200

    Improve perf_test.sh.

--- perf_test.sh
+++ perf_test.sh
@@ -1,7 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "This script destroys files. Remove these two lines to run the test."
 exit 1
+
+check_dependency() {
+    command -v "$1" >/dev/null 2>&1 || {
+        echo "Command $1 required, quitting" >&2
+        exit 1
+    }
+}
+for each in hyperfine find ; do
+    check_dependency "$each"
+done
 
 dirpath="/tmp/fdated_perf"
 echo "Recreate dir $dirpath (could take a couple of secs)"
@@ -18,8 +28,14 @@ do
     do
         monthDir="${yearDir}/${month}"
         mkdir $monthDir
-        touch ${monthDir}/main_${year}-${month}-{01..28}.log
+        touch "${monthDir}/main_${year}-${month}-{01..28}.log"
     done
 done
-echo "Find 4 files among `ls $dirpath | wc -l`"
-time ./fdated.py -n 100 -o 95 -t 2000-01-01 $dirpath >/dev/null
+echo "Find 4 files among $(ls $dirpath | wc -l) directories"
+echo " --- Python"
+options="-n 100 -o 95 -t 2000-01-01 $dirpath"
+hyperfine "./fdated.py $options >/dev/null"
+echo " --- Go"
+hyperfine "go-find-dated $options >/dev/null"
+echo " --- GNU find"
+hyperfine "find $dirpath -type f >/dev/null"
